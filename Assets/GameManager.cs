d4c7582bd05a034c046ac51921571567c3202731
EjaazLakhani1 <ejaaz.lakhani1@ucalgary.ca>
2021-03-13 15:33:46 -0700

    added multiple cubes and random locations

---
+++ Assets/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cubePrefab;
+
+    void Start()
+    {
+        InvokeRepeating("MoveCube", 0.0f, 2.0f);
+
+    }
+
+    // Update is called once per frame
+
+    int id = 0;
+    void MoveCube() 
+    {
+        float rand = Random.Range(-5.0f, 5.0f);
+        float pick = Random.Range(0,10.0f);
+        Vector3 position;
+        if (pick < 5.0f) {
+            if (pick < 2.5f) {
+                position = new Vector3(rand, 1.0f, 4.5f);
+            }
+            else {
+                position = new Vector3(rand, 1.0f, -4.5f);
+            }
+        }
+        else {
+            if (pick < 7.5f) {
+                position = new Vector3(4.5f, 1.0f, rand);
+            }
+            else {
+                position = new Vector3(-4.5f, 1.0f, rand);
+            }
+        }
+        Quaternion rotation = Quaternion.identity;
+        GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
+        cube.name = "Cube";
+        id++;
+    }
+
+    void Update()
+    {
+        //GameObject cube0 = GameObject.Find("Cube-0");
+        
+
+
+    }
+}
